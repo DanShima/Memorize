71969cfd8ca51c8d645216dd612ff3a8d67cca8e
DanShima <giddy_with_panda@hotmail.com>
2018-02-28 22:52:32 +0100

    make a button

--- app/src/main/java/com/danshima/memorize/MainActivity.kt
+++ app/src/main/java/com/danshima/memorize/MainActivity.kt
@@ -2,11 +2,17 @@ package com.danshima.memorize
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButton4Clicked(v: View){
+        Toast.makeText(this, "You clicked me!", Toast.LENGTH_LONG).show()
     }
 }
